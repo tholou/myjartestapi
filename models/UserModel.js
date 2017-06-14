46031160ebf2034ae509eacc79c99f1417894da1
Eyinade Tolu <tholou4reel@gmail.com>
2017-06-14 11:53:55 +0300

    added validation and some more development

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,3 +1,25 @@
 /**
  * Created by tholo on 6/14/2017.
  */
+
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var UserSchema = new Schema({
+    email: {
+        type: String,
+        Required: 'Kindly enter your email'
+    },
+    phone: {
+        type: String,
+        Required: 'Kindly enter your phone'
+    },
+    Created_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Users', UserSchema);
--- server.js
+++ server.js
@@ -4,7 +4,45 @@
 
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    User = require('./models/UserModel'),
+    bodyParser = require('body-parser');
+    ev = require('express-validation');
+
+
+
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./routes/UserRoutes');
+routes(app);
+app.use(function(req, res, next) {
+    res.status(404).send({url: req.originalUrl + ' not found'});
+    next();// move onto next middleware
+});
+// error handler
+app.use(function (err, req, res, next) {
+    // specific for validation errors
+    if (err instanceof ev.ValidationError){
+        // assign options
+        ev.options({
+            status: 422,
+            statusText: 'Unprocessable Entity'
+        });
+        return res.status(err.status).json(err);
+    } else {
+        res.status(500)
+            .json({
+                status: err.status,
+                message: err.message
+            });
+    }
+});
 
 app.listen(port);
 
